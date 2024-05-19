c07fce666de6a7896475efd9a4fd85de72e5477c
arpine.barseghyan <arpine.barseghyan@digitain.com>
2024-05-19 23:19:19 +0400

    [feat]: search input

--- test-task/src/components/Header/index.jsx
+++ test-task/src/components/Header/index.jsx
@@ -5,13 +5,42 @@ import { Logo } from "../../assets/svgs/Logo";
 import { Search } from "../../assets/svgs/search";
 
 function Header() {
+  const [showSearchInput, setShowSearchInput] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+
+  function handleSearchIconClick() {
+    setShowSearchInput(true);
+  }
+
+  function handleInputBlur() {
+    setShowSearchInput(false);
+  }
+
+  function handleSearchInputChange(event) {
+    const { value } = event.target;
+    setSearchValue(value);
+  }
+
   return (
     <div className={styles.LogoAndSearch}>
       <div className={styles.LogoContainer}>
         <Logo />
       </div>
-      <div className={styles.SearchButton}>
-        <Search />
+      <div className={styles.Search}>
+        {!showSearchInput && (
+          <div onClick={handleSearchIconClick} className={styles.SearchButton}>
+            <Search />
+          </div>
+        )}
+        {showSearchInput && (
+          <input
+            name="search"
+            value={searchValue}
+            autoFocus
+            onChange={handleSearchInputChange}
+            onBlur={handleInputBlur}
+          />
+        )}
       </div>
     </div>
   );
